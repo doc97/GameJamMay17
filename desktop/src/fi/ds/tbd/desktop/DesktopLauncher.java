9f16cdee8ca3e5dafdd59e090e3e846b85149dff
doc97 <daniel.riissanen@gmail.com>
2017-05-26 18:05:51 +0300

    Add libGDX project files

---
+++ desktop/src/fi/ds/tbd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.ds.tbd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.ds.tbd.TBD;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TBD(), config);
+	}
+}
